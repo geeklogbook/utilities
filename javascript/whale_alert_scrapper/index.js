a4a5f51e24488e03ca6663a77644d079cdfac393
jino.michel.aque <contact@geeklogbook.com>
2022-11-10 17:54:49 -0300

    add fetched function

--- javascript/whale_alert_scrapper/index.js
+++ javascript/whale_alert_scrapper/index.js
@@ -1,43 +1,52 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
+const cheerio = require("cheerio"); 
+const fetch = require('node-fetch')
 
 async function run() {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://whale-alert.io/whales');
+    // const browser = await puppeteer.launch();
+    // const page = await browser.newPage();
+    // await page.goto('https://whale-alert.io/whales');
 
-    // await page.screenshot({path: 'example.png'})
-
-    // const html = await page.content();
-    // console.log(html)
-
-    // const title = await page.evaluate(() => document.title);
-    // console.log(title)
- 
-    // const table = await page.evaluate(() => document.table);
-    // console.log(table)
-
-    // const links = await page.evaluate(() => Array.from(document.querySelectorAll('a'), (e) =>e.href))
-    // console.log(links)
-
-     //const currencyTable = await page.evaluate(() => Array.from(document.querySelectorAll('table tr td'), (e) =>e.innerText))
-     //console.log(currencyTable)
-     const currencyTable = await page.evaluate(() => 
-        Array.from(document.querySelectorAll('table tbody tr'), (e) => ({
-            currency: e.querySelector('i').innerText,
-            Know: e.querySelector(':nth-child(2)').innerText,
-            Unknown: e.querySelector(':nth-child(3)').innerText,
-        }))
-    );
-    console.log(currencyTable)
+    // const currencyTableScrapped = await page.evaluate(() => 
+    //     [...document.querySelectorAll("table tbody tr")].map(e => {
+    //     const cells = e.querySelectorAll("td");
+    //     return {
+    //         currency: cells[0].textContent.trim(),
+    //         Know: cells[1].textContent.trim(),
+    //         Unknown: cells[2].textContent.trim(),
+    //         };
+    //     })
+    // );
+    // console.log(currencyTableScrapped);
+    // await browser.close();
     
+
     // fs.writeFile('currencyTable.json', JSON.stringify(currencyTable), (err) => {
     //     if (err) throw err;
     //     console.log('object')
     // })
-
-
-    await browser.close();
+    var currencyTableFetched;
+    fetch("https://whale-alert.io/whales") 
+        .then(res => res.text())
+        .then(html => {
+            const $ = cheerio.load(html);
+            const rows = [...$("table tr")].slice(1).map((e, i) => {
+            const cells = $(e).find("td");
+        return {
+            currency: $(cells[0]).text().trim(),
+            Know: $(cells[1]).text().trim(),
+            Unknown: $(cells[2]).text().trim(),
+        };
+        })
+        currencyTableFetched = rows;
+        console.log(rows);
+        // fs.writeFile('currencyTableFetched.json', JSON.stringify(rows), (err) => {
+        //     if (err) throw err;
+        //     console.log('object')
+        // })
+    });
+    console.log(currencyTableFetched)
 }
 
 run();
