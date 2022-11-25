866d4e1e5442c241de9abfa45e73e8a8ebb9e22d
jino.michel.aque <contact@geeklogbook.com>
2022-11-25 10:18:37 -0300

    add test_password practice

---
+++ python/test_pasword.py
@@ -0,0 +1,36 @@
+special_characters = ['&', '#', '$', '!', '?', '"', '(', ')', '.']
+
+alphabetic_characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Define fixed values here
+password = input("Create password: ")
+print(password)
+print(len(password))
+len_password = len(password)
+count_alphabetic_characters = password.count(alphabetic_characters)
+count_special_characters = password.count(special_characters)
+count_numbers = password.count(numbers)
+
+# Write your code here
+while password:
+    if len_password < 8:
+        if count_alphabetic_characters < 3:
+            if count_special_characters < 3:
+                if count_numbers < 2:
+                    print("You need at least 2 number digits!")
+                    break
+                print("you need at least 3 special characters!")
+                break
+            print("alphabetics must be at least 3!")
+            break
+        print("length must be at least 8 characters!")
+        break
+# if (len(password)<8):
+#     user_password=list(password)
+#     if any(i in user_password for i in alphabetic_characters if )>=3:
+
+#     print("password must be at least 8 char!")
+#     break
