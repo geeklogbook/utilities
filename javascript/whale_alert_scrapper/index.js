9b9f27d2f607c61aef5a7524017cba88c3f2a314
jino.michel.aque <contact@geeklogbook.com>
2023-01-03 11:50:12 -0300

    add random practices

--- javascript/whale_alert_scrapper/index.js
+++ javascript/whale_alert_scrapper/index.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
-const cheerio = require("cheerio"); 
-const fetch = require('node-fetch')
+const cheerio = require("cheerio");
+const fetch = require('node-fetch');
+const { table } = require('console');
 
 async function run() {
+    // SCRAPPING
     // const browser = await puppeteer.launch();
     // const page = await browser.newPage();
     // await page.goto('https://whale-alert.io/whales');
-
     // const currencyTableScrapped = await page.evaluate(() => 
     //     [...document.querySelectorAll("table tbody tr")].map(e => {
     //     const cells = e.querySelectorAll("td");
@@ -20,33 +21,44 @@ async function run() {
     // );
     // console.log(currencyTableScrapped);
     // await browser.close();
-    
 
     // fs.writeFile('currencyTable.json', JSON.stringify(currencyTable), (err) => {
     //     if (err) throw err;
     //     console.log('object')
     // })
-    var currencyTableFetched;
-    fetch("https://whale-alert.io/whales") 
-        .then(res => res.text())
-        .then(html => {
-            const $ = cheerio.load(html);
-            const rows = [...$("table tr")].slice(1).map((e, i) => {
-            const cells = $(e).find("td");
-        return {
-            currency: $(cells[0]).text().trim(),
-            Know: $(cells[1]).text().trim(),
-            Unknown: $(cells[2]).text().trim(),
-        };
-        })
-        currencyTableFetched = rows;
-        console.log(rows);
-        // fs.writeFile('currencyTableFetched.json', JSON.stringify(rows), (err) => {
-        //     if (err) throw err;
-        //     console.log('object')
-        // })
-    });
-    console.log(currencyTableFetched)
+
+    //FETCHING
+    // fetch("https://whale-alert.io/whales") 
+    //     .then(res => res.text())
+    //     .then(html => {
+    //         const $ = cheerio.load(html);
+    //         const rows = [...$("table tr")].slice(1).map((e, i) => {
+    //         const cells = $(e).find("td");
+    //     return {
+    //         currency: $(cells[0]).text().trim(),
+    //         Know: $(cells[1]).text().trim(),
+    //         Unknown: $(cells[2]).text().trim(),
+    //     };
+    //     })
+    //     console.table(rows);
+    // });
+
+    // var nombre = async function (username){
+    //     const url = `https://api.github.com/users/${username}`
+    //     var respuesta = await fetch (url)
+    //     var response = await respuesta.json();
+    //     return response.login;        
+    // }
+
+    async function currencyTableFetched(url) {
+        var result = await fetch(url);
+        var response = await result.text()
+        return response
+    }
+
+    var table = currencyTableFetched('https://whale-alert.io/whales')
+
+    console.log(table)
 }
 
 run();
---
+++ python/logger_first_approach.py
@@ -0,0 +1,4 @@
+import logging
+
+logging.warning("Ey, this is a logging warning")
+logging.error("Ey, this is a logging error")
