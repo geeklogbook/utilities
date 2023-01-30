87481344c2d88351a054eebbda7aea866f9c12ac
jino.michel.aque <contact@geeklogbook.com>
2023-01-30 00:31:57 -0300

    add log snippets

--- python/logger_first_approach.py
+++ python/logger_first_approach.py
@@ -1,4 +1,13 @@
+"""
+Using the python log subsystem
+"""
+
 import logging
 
+logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
 logging.warning("Ey, this is a logging warning")
 logging.error("Ey, this is a logging error")
+logging.debug('This message should go to the log file')
+logging.info('So should this')
+logging.warning('And this, too')
+logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
