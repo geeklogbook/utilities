026be268db32441c52d39f5cf33741385f032d0d
jino.michel.aque <contact@geeklogbook.com>
2023-02-10 00:05:26 -0300

    reorder python utilities

--- python/faker/exist_provider.py
+++ python/faker/exist_provider.py
@@ -5,10 +5,7 @@ fake = Faker()
 
 def random_elements(size:int) -> list:
     methods_list = [method for method in dir(fake) if method.startswith('_') is False]
-
-
     random_elements = random.choices(methods_list, k=size)
-
     return random_elements
     
 randoms = random_elements(4)
@@ -16,5 +13,4 @@ randoms = random_elements(4)
 
 for elem in randoms:
     print(elem, type(elem))
-
     print(getattr(fake, elem)())
