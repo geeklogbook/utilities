1e025e1feb1d7202f0fb34d0753ce1692ad21254
jino.michel.aque <contact@geeklogbook.com>
2023-09-30 00:09:33 -0300

    update data raw

--- python/miscellaneous/ar_election/arg_final_counting.py
+++ python/miscellaneous/ar_election/arg_final_counting.py
@@ -8,27 +8,30 @@ charge_blank = []
 
 #Error: Capital Federal, Buenos Aires, Santiago del Estero
 #provinces = ['CATAMARCA', 'CORDOBA', 'CORRIENTES', 'CHACO', 'CHUBUT', 'ENTRE RIOS', 'FORMOSA', 'JUJUY', 'LA PAMPA', 'LA RIOJA', 'MENDOZA', 'MISIONES', 'NEUQUEN', 'RIO NEGRO', 'SALTA', 'SAN JUAN', 'SAN LUIS', 'SANTA CRUZ', 'SANTA FE', 'TUCUMAN', 'TIERRA DEL FUEGO']
-provinces = ['SALTA', 'SAN JUAN', 'SAN LUIS', 'SANTA CRUZ', 'SANTA FE', 'TUCUMAN', 'TIERRA DEL FUEGO']
+provinces = ['CAPITAL FEDERAL', 'BUENOS AIRES', 'SANTIAGO DEL ESTERO']
 charge = ['DIPUTADO NACIONAL', 'PARLAMENTARIO MERCOSUR DN', 'PARLAMENTARIO MERCOSUR DR', 'PRESIDENTE Y VICE']
 
-for province in provinces:
-    print(province)
-    driver = webdriver.Firefox()
-    driver.get("https://www.padron.gob.ar/publica/")
-    driver.find_element(By.ID, "site").click()
-    time.sleep(5)
-    select_site = Select(driver.find_element(By.ID, "site"))
-    select_site.select_by_visible_text(province) # Select Distrito Electoral
-    driver.find_element(By.ID, "tabgreen").click()
-    time.sleep(10)
-    driver.find_element(By.ID, "carg").click() # Select Consultar Cargo
-    select_cargo = Select(driver.find_element(By.ID, "carg"))
-    select_cargo.select_by_visible_text('PRESIDENTE Y VICE')
-    driver.find_element(By.ID, "btnVer").click()
-    time.sleep(10)
-    driver.find_element(By.ID, "btnExcel").click() #"Export File
-    time.sleep(10)
-    driver.quit()
+for c in charge:
+    print(c)
+    for province in provinces:
+        print(province)
+        driver = webdriver.Firefox()
+        driver.get("https://www.padron.gob.ar/publica/")
+        driver.find_element(By.ID, "site").click()
+        time.sleep(5)
+        select_site = Select(driver.find_element(By.ID, "site"))
+        select_site.select_by_visible_text(province) # Select Distrito Electoral
+        time.sleep(5)
+        driver.find_element(By.ID, "tabgreen").click()
+        time.sleep(10)
+        driver.find_element(By.ID, "carg").click() # Select Consultar Cargo
+        select_cargo = Select(driver.find_element(By.ID, "carg"))
+        select_cargo.select_by_visible_text(c)
+        driver.find_element(By.ID, "btnVer").click()
+        time.sleep(10)
+        driver.find_element(By.ID, "btnExcel").click() #"Export File
+        time.sleep(10)
+        driver.quit()
 
 
 #GIVE ME A LIST WITH PROVINCES AND CHARGES
