29f4de805abfdd2b4cdb417d8a48540d550e6931
geeklogbook <contact@geeklogbook.com>
2024-05-19 21:06:50 -0300

    C Fun with Kernighan and Ritchie

---
+++ c/the_book/hello_world.c
@@ -0,0 +1,19 @@
+//Page 9 first program
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World\n");
+    return 0;
+}
+
+//Excercise 1-1 - Run the program in your sistem. 
+
+
+//Excercise 1-2 (Escape sequence) use an argument that not is not in list
+
+// int main()
+// {
+//     printf("Hello, World\c"); ERROR -> unknown escape sequence: '\c'
+//     return 0;
+// }
