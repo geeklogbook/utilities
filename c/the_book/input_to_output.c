6a6838797cecdefe6c4317f9d7ae8cd094198877
geeklogbook <contact@geeklogbook.com>
2024-09-14 00:58:17 -0300

    C Fun

---
+++ c/the_book/input_to_output.c
@@ -0,0 +1,14 @@
+//Page 18
+#include <stdio.h>
+
+int main()
+{
+    int c;
+    c = getchar();
+    while (c != EOF)
+    {
+        putchar(c);
+        c = getchar();
+    }
+  
+}
---
+++ c/the_book/input_to_output_c.c
@@ -0,0 +1,15 @@
+// Excercise 1-6 - Verify that the expression getchar() != EOF is 0 or 1 
+
+#include <stdio.h>
+
+int main()
+{
+    int c;
+
+    while (c=getchar() != EOF)
+    {
+        printf("%d\n",c);
+    }
+
+    return 0;
+}
